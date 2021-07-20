6d2d69e04fd2d528f0d0de99a1d94c656d840cb7
Максим <xalyes@gmail.com>
2021-07-20 21:21:14 +0300

    Little endian fixed

--- src/leaf.cpp
+++ src/leaf.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <array>
+#include <boost/endian/conversion.hpp>
 
 #include "leaf.h"
 
@@ -20,8 +21,20 @@ void Leaf<std::string>::Load()
     in.read(reinterpret_cast<char*>(&(m_keyCount)), sizeof(m_keyCount));
     in.read(reinterpret_cast<char*>(&(m_keys)), sizeof(m_keys));
 
+    boost::endian::little_to_native_inplace(m_keyCount);
+
+    for (uint32_t i = 0; i < m_keys.size(); i++)
+    {
+        boost::endian::little_to_native_inplace(m_keys[i]);
+    }
+
     std::array<Offset, MaxKeys> offsets;
     in.read(reinterpret_cast<char*>(&(offsets)), sizeof(offsets));
+
+    for (uint32_t i = 0; i < offsets.size(); i++)
+    {
+        boost::endian::little_to_native_inplace(offsets[i]);
+    }
 
     for (uint32_t i = 0; i < m_keyCount; i++)
     {
@@ -42,6 +55,7 @@ void Leaf<std::string>::Load()
     }
 
     in.read(reinterpret_cast<char*>(&(m_nextBatch)), sizeof(m_nextBatch));
+    boost::endian::little_to_native_inplace(m_nextBatch);
 }
 
 template<>
@@ -52,17 +66,23 @@ void Leaf<std::string>::Flush()
     out.open(m_dir / ("batch_" + std::to_string(m_index) + ".dat"), std::ios::out | std::ios::binary | std::ios::trunc);
 
     out.write("9", 1);
-    out.write(reinterpret_cast<char*>(&(m_keyCount)), sizeof(m_keyCount));
-    out.write(reinterpret_cast<char*>(&(m_keys)), sizeof(m_keys));
+
+    auto keyCount = boost::endian::native_to_little(m_keyCount);
+    out.write(reinterpret_cast<char*>(&keyCount), sizeof(keyCount));
+
+    for (uint32_t i = 0; i < m_keys.size(); i++)
+    {
+        auto key = boost::endian::native_to_little(m_keys[i]);
+        out.write(reinterpret_cast<char*>(&key), sizeof(key));
+    }
 
     std::array<Offset, MaxKeys> offsets;
     offsets.fill(0);
 
     auto offsetPos = out.tellp();
     offsets[0] = sizeof(m_keyCount) + sizeof(m_keys) + sizeof(offsets) + 1;
-    out.write(reinterpret_cast<char*>(&(offsets)), sizeof(offsets));
+    out.write(reinterpret_cast<char*>(&offsets), sizeof(offsets));
 
-    uint32_t i = 0;
     for (uint32_t i = 0; i < m_values.size(); i++)
     {
         out.write(m_values[i].data(), m_values[i].size());
@@ -70,10 +90,16 @@ void Leaf<std::string>::Flush()
             offsets[i] = offsets[i - 1] + m_values[i - 1].size();
     }
 
-    out.write(reinterpret_cast<char*>(&(m_nextBatch)), sizeof(m_nextBatch));
+    auto nextBatch = boost::endian::native_to_little(m_nextBatch);
+    out.write(reinterpret_cast<char*>(&(nextBatch)), sizeof(nextBatch));
 
     out.seekp(offsetPos);
-    out.write(reinterpret_cast<char*>(&(offsets)), sizeof(offsets));
+
+    for (uint32_t i = 0; i < offsets.size(); i++)
+    {
+        auto o = boost::endian::native_to_little(offsets[i]);
+        out.write(reinterpret_cast<char*>(&o), sizeof(o));
+    }
 }
 
 } // kv_storage
--- src/node.cpp
+++ src/node.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
 
+#include <boost/endian/conversion.hpp>
+
 #include <kv_storage/volume.h>
 
 namespace kv_storage {
@@ -17,6 +19,15 @@ void Node<std::string>::Load()
     in.read(reinterpret_cast<char*>(&(m_keyCount)), sizeof(m_keyCount));
     in.read(reinterpret_cast<char*>(&(m_keys)), sizeof(m_keys));
     in.read(reinterpret_cast<char*>(&(m_ptrs)), sizeof(m_ptrs));
+
+    boost::endian::little_to_native_inplace(m_keyCount);
+
+    for (uint32_t i = 0; i < m_keys.size(); i++)
+    {
+        boost::endian::little_to_native_inplace(m_keys[i]);
+        boost::endian::little_to_native_inplace(m_ptrs[i]);
+    }
+    boost::endian::little_to_native_inplace(m_ptrs[m_ptrs.size() - 1]);
 }
 
 template<>
@@ -28,9 +39,20 @@ void Node<std::string>::Flush()
 
     out.write("8", 1);
 
-    out.write(reinterpret_cast<char*>(&(m_keyCount)), sizeof(m_keyCount));
-    out.write(reinterpret_cast<char*>(&(m_keys)), sizeof(m_keys));
-    out.write(reinterpret_cast<char*>(&(m_ptrs)), sizeof(m_ptrs));
+    auto keyCount = boost::endian::native_to_little(m_keyCount);
+    out.write(reinterpret_cast<char*>(&keyCount), sizeof(keyCount));
+
+    for (uint32_t i = 0; i < m_keys.size(); i++)
+    {
+        auto key = boost::endian::native_to_little(m_keys[i]);
+        out.write(reinterpret_cast<char*>(&key), sizeof(key));
+    }
+
+    for (uint32_t i = 0; i < m_ptrs.size(); i++)
+    {
+        auto ptr = boost::endian::native_to_little(m_ptrs[i]);
+        out.write(reinterpret_cast<char*>(&ptr), sizeof(ptr));
+    }
 }
 
 } // kv_storage
