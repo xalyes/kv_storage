0e84bf5f85cf7f177c6507185aa8baa2c43b4c7a
Максим <xalyes@gmail.com>
2021-07-25 13:28:33 +0300

    Added priority for mounted volumes

--- include/kv_storage/storage.h
+++ include/kv_storage/storage.h
@@ -1,6 +1,8 @@
 #ifndef KV_STORAGE_STORAGE_H
 #define KV_STORAGE_STORAGE_H
 
+#include <map>
+
 #include "volume.h"
 
 namespace kv_storage {
@@ -10,14 +12,14 @@ class StorageNode
 {
 public:
     StorageNode() {}
-    void Mount(const Volume<V>& vol, FileIndex idx = 1);
+    void Mount(const Volume<V>& vol, size_t priority = 0, FileIndex idx = 1);
     std::vector<V> Get(const Key& key) const;
     std::vector<std::shared_ptr<StorageNode<V>>> GetChilds() const;
     std::shared_ptr<StorageNode<V>> CreateChildNode();
     void EraseNode(size_t idx);
 
 private:
-    std::vector<std::shared_ptr<BPNode<V>>> m_volumeNodes;
+    std::multimap<size_t, std::shared_ptr<BPNode<V>>> m_volumeNodes;
     std::vector<std::shared_ptr<StorageNode<V>>> m_childs;
 };
 
@@ -45,21 +47,24 @@ void StorageNode<V>::EraseNode(size_t idx)
 }
 
 template<class V>
-void StorageNode<V>::Mount(const Volume<V>& vol, FileIndex idx)
+void StorageNode<V>::Mount(const Volume<V>& vol, size_t priority, FileIndex idx)
 {
-    m_volumeNodes.push_back(vol.GetCustomNode(idx));
+    m_volumeNodes.insert({ priority, vol.GetCustomNode(idx) });
 }
 
 template<class V>
 std::vector<V> StorageNode<V>::Get(const Key& key) const
 {
     std::vector<V> values;
-    std::optional<V> valueFromThis;
+    std::optional<V> foundValue;
     for (const auto& vol : m_volumeNodes)
     {
-        const auto res = vol->Get(key);
+        const auto res = vol.second->Get(key);
         if (res)
-            valueFromThis = res;
+        {
+            foundValue = res;
+            break;
+        }
     }
 
     for (const auto& child : m_childs)
@@ -67,8 +72,8 @@ std::vector<V> StorageNode<V>::Get(const Key& key) const
         const auto res = child->Get(key);
         values.insert(values.end(), res.begin(), res.end());
     }
-    if (valueFromThis)
-        values.push_back(*valueFromThis);
+    if (foundValue)
+        values.push_back(*foundValue);
     return values;
 }
 
